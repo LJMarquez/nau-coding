99157ffd978cd5fcfec2ce3fa79c1b303e845d63
Leo Marquez <marquez.leo100@gmail.com>
2025-10-06 10:47:17 -0700

    finished lab

--- CS-126L/partB_random_guess.py
+++ CS-126L/partB_random_guess.py
@@ -10,9 +10,9 @@ def main():
 
     # A sentinel loop to keep playing after each game is done
     print("A haiku for you:")
-    print("Numbers hide from your eyes,")
-    print("Think hard to get a great guess,")
-    print("Think you can do it?")
+    print("Hidden numbers wait,")
+    print("Guess with logic and some luck,")
+    print("Will you guess it right?")
 
     total_games = 0
     guess_list = []
@@ -22,24 +22,9 @@ def main():
         total_games += 1
         guess_list.append(start_game())
         play_again = input("Do you want to play again? ")
+        play_again = play_again.lower()
 
     total_guesses = sum(guess_list)
-
-        # user_guess = int(input("Your guess? "))
-        # number_of_guesses += 1
-        # if user_guess == random_number:
-        #     print(f"You got it right in {number_of_guesses} guesses!")
-        #     play_again_bool = input("Do you want to play again? ")
-
-        #     if play_again_bool.startswith("y"):
-        #         start_game()
-        #     else:
-        #         print("Game Over!")
-
-        # elif user_guess > random_number:
-        #     print("It's lower.")
-        # else:
-        #     print("It's higher.")
 
     print_results( total_games, total_guesses, guess_list )
 
@@ -62,7 +47,14 @@ def start_game():
 
     while user_guess != random_number:
 
-        user_guess = int(input("Your guess? "))
+        while user_guess < 1 or user_guess > 100:
+            try:
+                user_guess = int(input("Your guess? "))
+                if user_guess < 1 or user_guess > 100:
+                    print("Please enter a number between 1-100")
+            except ValueError:
+                print("Please enter a number between 1-100")
+            
 
         number_of_guesses += 1
         if user_guess == random_number:
@@ -73,6 +65,8 @@ def start_game():
             print("It's lower.")
         else:
             print("It's higher.")
+
+        user_guess = 0
             
 # A function that displays
 def print_results(total_games, total_guesses, guess_list):
@@ -84,6 +78,7 @@ def print_results(total_games, total_guesses, guess_list):
     guesses_per_game = round(total_guesses / total_games, 1)
     guess_list.sort()
 
+    print()
     print("Overall results:")
     print(f"Total Games = {total_games}")
     print(f"Total Guesses = {total_guesses}")
